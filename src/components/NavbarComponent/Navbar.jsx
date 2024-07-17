4802dcb0db3ab368950eb980279b449a244fce15
AbedinTelaku <abedintelaku1@gmail.com>
2024-07-17 14:01:34 +0200

    Added media query

--- src/components/NavbarComponent/Navbar.jsx
+++ src/components/NavbarComponent/Navbar.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
-import "./navbar.css";
+import React, { useState } from 'react';
+import './navbar.css';
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li className='components'><Link to="/">Home</Link></li>
-        <li className='components'><Link to="/about">About</Link></li>
-        <li className='components'><Link to="/company">Company</Link></li>
-        <li className='components'><Link to="/contact">Contact us</Link></li>
+    <nav className="navbar">
+      <div className="navbar-hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+      <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
+        <li className='components'><Link to="/" onClick={toggleMenu}>Home</Link></li>
+        <li className='components'><Link to="/about" onClick={toggleMenu}>About</Link></li>
+        <li className='components'><Link to="/company" onClick={toggleMenu}>Company</Link></li>
+        <li className='components'><Link to="/contact" onClick={toggleMenu}>Contact us</Link></li>
       </ul>
     </nav>
   );
