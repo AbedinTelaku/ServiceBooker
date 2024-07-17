d50aa1fae50f598b49117480113d950a37e193d0
AbedinTelaku <abedintelaku1@gmail.com>
2024-07-17 13:43:08 +0200

    Added Contact Us

--- src/components/ContactUsComponent/Contact.jsx
+++ src/components/ContactUsComponent/Contact.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './contact.css';
 
-const Contact = () => {
+function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <form className="contact-form">
+        <label>
+          Name:
+          <input type="text" name="name" required />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          Message:
+          <textarea name="message" required></textarea>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
